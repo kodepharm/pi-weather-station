5438ba81ed663c9b8102364e147dd3b7d9d1a88b
Ehsan <ehsanbigzad7@gmail.com>
2024-05-28 22:47:40 +0430

    update fetch for v16

--- server/train.js
+++ server/train.js
@@ -1,21 +1,22 @@
 async function getTrain(req, res) {
+  const { default: fetch } = await import("node-fetch");
   try {
     const response = await fetch(
-        "https://api.goswift.ly/real-time/mta-maryland-marc/gtfs-rt-trip-updates?format=json",
-        {
-          headers: {
-            Authorization: "17fcc7fb43237c3379ab97ce79cfd808",
-          },
-        }
-      );
-    
-      const data = await response.json();
-      res.json(data);
+      "https://api.goswift.ly/real-time/mta-maryland-marc/gtfs-rt-trip-updates?format=json",
+      {
+        headers: {
+          Authorization: "17fcc7fb43237c3379ab97ce79cfd808",
+        },
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
   } catch (error) {
     res.json({
       status: "error",
-      message: error
-    })
+      message: error,
+    });
   }
 }
 
